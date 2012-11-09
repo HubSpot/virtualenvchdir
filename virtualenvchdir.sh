51bd326d3a15de08fbb3ea72dfa292c8104250b6
Mike Axiak <maxiak@hubspot.com>
2012-11-08 22:16:35 -0500

    Rather than check functions, just run and check.

--- virtualenvchdir.sh
+++ virtualenvchdir.sh
@@ -5,21 +5,10 @@
 ####
 
 
-_venv_is_function_defined() {
-    FUNCTION_NAME="$1"
-    if [ -n "$ZSH_VERSION" ]; then
-        functions | egrep -q "^$FUNCTION_NAME \(\)"
-        return $?
-    else
-        typeset -F | egrep -q "^declare -f $FUNCTION_NAME"
-        return $?
-    fi
-}
-
-
 _venv_quick_workon() {
     # Only run workon if the new virtualenv is different from this one.
     VIRTUALENV="$1"
+    echo $VIRTUALENV
     if [[ -z "$VIRTUAL_ENV" ]]; then
         workon $VIRTUALENV
         export _VIRTUALENV_AUTO_SET=1
@@ -44,11 +33,9 @@ _venv_change_virtualenv() {
         CURRENT_DIR=$(dirname "$CURRENT_DIR")
     done
 
-    if _venv_is_function_defined "deactivate"; then
-        if [[ -n "$_VIRTUALENV_AUTO_SET" ]]; then
-            deactivate
-            unset _VIRTUALENV_AUTO_SET
-        fi
+    if [[ -n "$_VIRTUALENV_AUTO_SET" ]]; then
+        deactivate &>/dev/null
+        unset _VIRTUALENV_AUTO_SET
     fi
 }
 
@@ -58,8 +45,10 @@ _venv_cd () {
 }
 
 
-if _venv_is_function_defined "workon"; then
-    alias cd="_venv_cd"
-else
+workon &>/dev/null
+
+if [ $? -eq 127 ]; then
     echo "Please use the excellent virtualenvwrapper library first!" 1>&2
+else
+    alias cd="_venv_cd"
 fi
